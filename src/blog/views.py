4bad1521d993a265e5a1bd50658934e18ac1de52
rayyan22207 <rayyan22207@gmail.com>
2025-07-07 23:41:51 +0500

    working on ui

--- src/blog/views.py
+++ src/blog/views.py
@@ -4,4 +4,9 @@ from .models import BlogPost
 # Create your views here.
 def home(request):
     posts = BlogPost.objects.all() 
+    for post in posts:
+        if post.image:
+            print(post.image.url)
+        else:
+            print('none')
     return render(request, 'home.html', {'posts': posts})
--- src/core/settings.py
+++ src/core/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+import os
 
 
 # Quick-start development settings - unsuitable for production
@@ -117,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
