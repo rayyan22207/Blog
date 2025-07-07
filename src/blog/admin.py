9707e04daf7f404e5cb4e926c8d338607ef3e6de
rayyan22207 <rayyan22207@gmail.com>
2025-07-07 17:01:16 +0500

    working on the models and admin

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import BlogPost, Comment, Like
 
 # Register your models here.
+admin.site.register(BlogPost)
+admin.site.register(Comment)
+admin.site.register(Like)
--- src/blog/models.py
+++ src/blog/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
 
 # Create your models here.
 class BlogPost(models.Model):
@@ -27,3 +30,18 @@ class Comment(models.Model):
 
     def __str__(self):
         return f'Comment by {self.user.username if self.user else "Deleted User"}'
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    liked_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'content_type', 'object_id')  # Prevent duplicate likes
+
+    def __str__(self):
+        return f"{self.user.username} liked {self.content_object}"
