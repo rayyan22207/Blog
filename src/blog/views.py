99a4693214f1618478374f3a142470880d3763d0
rayyan22207 <rayyan22207@gmail.com>
2025-07-07 20:37:40 +0500

    working on

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import BlogPost
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {'home': 'hello'})
+    posts = BlogPost.objects.all() 
+    return render(request, 'home.html', {'posts': posts})
